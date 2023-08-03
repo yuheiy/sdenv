929d9927041ddac9488b9b825448bb413150821a
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2023-08-04 01:09:15 +0900

    install @astrojs/sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -6,7 +7,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
   vite: {
     define: {
       'import.meta.vitest': 'undefined',
