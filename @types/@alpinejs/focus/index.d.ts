49fabf2091007397e228ccf712c39604139fa479
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-01-22 16:18:49 +0900

    configure Linting with Type Information

--- @types/@alpinejs/focus/index.d.ts
+++ @types/@alpinejs/focus/index.d.ts
@@ -1,1 +1,5 @@
-declare module '@alpinejs/focus';
+declare module '@alpinejs/focus' {
+  import type { PluginCallback } from 'alpinejs';
+  declare const focus: PluginCallback;
+  export default focus;
+}
