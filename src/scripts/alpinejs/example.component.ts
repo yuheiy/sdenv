2cb4b7c266e767b0d2eeb5f2bf30ea4e8d6fe944
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-02-10 00:35:54 +0900

    use outvariant instead of tiny-invariant

--- src/scripts/alpinejs/example.component.ts
+++ src/scripts/alpinejs/example.component.ts
@@ -1,9 +1,9 @@
 import Alpine from 'alpinejs';
-import invariant from 'tiny-invariant';
+import { invariant } from 'outvariant';
 
 Alpine.data('example', (initialValue = 0) => {
   // ensure that `initialValue` is of type number
-  invariant(typeof initialValue === 'number');
+  invariant(typeof initialValue === 'number', 'Invariant failed');
   // then `initialValue` is inferred as a Number value
 
   /**
