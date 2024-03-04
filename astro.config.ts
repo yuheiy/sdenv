c83e4c36ff55723d71c9de276c27521fe9b27068
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-03-05 07:51:02 +0900

    install astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({ nesting: true })],
+  integrations: [mdx(), sitemap(), tailwind({ nesting: true }), icon()],
   vite: {
     define: {
       'import.meta.vitest': 'undefined',
