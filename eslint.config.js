34b6f79bfd342edc2124a7075698ca7f3d70a73a
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-05-24 01:55:23 +0900

    update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,6 @@
 import astro from 'eslint-plugin-astro';
 import tailwindcss from 'eslint-plugin-tailwindcss';
 
-const tailwindcssConfig = {
-  languageOptions: {
-    parserOptions: tailwindcss.configs.recommended.parserOptions,
-  },
-  plugins: {
-    tailwindcss,
-  },
-  rules: tailwindcss.configs.recommended.rules,
-};
-
 export default [
   // globally ignoring
   {
@@ -28,7 +18,7 @@ export default [
   ...astro.configs['flat/jsx-a11y-recommended'],
 
   // eslint-plugin-tailwindcss
-  tailwindcssConfig,
+  ...tailwindcss.configs['flat/recommended'],
   {
     rules: {
       // use prettier-plugin-tailwindcss for class sorting
