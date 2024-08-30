e5ce02774aca76f67541eca03b6b21366b033b4d
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-08-31 01:01:37 +0900

    uninstall rollup-plugin-visualizer

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,9 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind({ nesting: true }), icon()],
-  vite: {
-    plugins: [visualizer()],
-  },
 });
