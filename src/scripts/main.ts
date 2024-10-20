a0ec55bf90e05e6fcbca8976e619f3c2463f2191
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-10-20 21:48:51 +0900

    unwrap main in main.ts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,22 +2,16 @@ import Alpine from 'alpinejs';
 import './components';
 import './stores';
 
-async function main() {
-  // https://vitejs.dev/guide/env-and-mode.html#env-variables
-  if (import.meta.env.DEV) {
-    console.log({
-      MODE: import.meta.env.MODE,
-      BASE_URL: import.meta.env.BASE_URL,
-      PROD: import.meta.env.PROD,
-      DEV: import.meta.env.DEV,
-    });
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (window as any).Alpine = Alpine;
-  Alpine.start();
+// https://vitejs.dev/guide/env-and-mode.html#env-variables
+if (import.meta.env.DEV) {
+  console.log({
+    MODE: import.meta.env.MODE,
+    BASE_URL: import.meta.env.BASE_URL,
+    PROD: import.meta.env.PROD,
+    DEV: import.meta.env.DEV,
+  });
 }
 
-main().catch((e) => {
-  throw e;
-});
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).Alpine = Alpine;
+Alpine.start();
