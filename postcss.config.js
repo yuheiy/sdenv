666a79e51196a1d5a48995fad0caa3d21f094f12
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-02-09 18:44:56 +0900

    load breakpoints via tailwind in postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,26 @@
-// FIXME: tailwindから読み込むようにする
-const breakpoints = {
-  sm: '40rem',
-  md: '48rem',
-  lg: '64rem',
-  xl: '80rem',
-  '2xl': '96rem',
-};
+import { __unstable__loadDesignSystem } from '@tailwindcss/node';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+
+async function loadBreakpoints(cssPath) {
+  const css = await fsPromises.readFile(cssPath, 'utf8');
+  const { theme } = await __unstable__loadDesignSystem(css, {
+    base: path.dirname(cssPath),
+  });
+
+  const breakpoints = {};
+
+  for (const [key, { value }] of theme.values.entries()) {
+    const prefix = '--breakpoint-';
+    if (key.startsWith(prefix)) {
+      breakpoints[key.replace(prefix, '')] = value;
+    }
+  }
+
+  return breakpoints;
+}
+
+const breakpoints = await loadBreakpoints('./src/styles/global.css');
 
 /** @type {import('postcss-load-config').Config} */
 export default {
