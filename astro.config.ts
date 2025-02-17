590f8d42fb2cc8f8214093c253ccb9cbac15c2a3
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-02-17 10:59:42 +0900

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
+import fluidVisitor from './lightningcss-plugin-fluid-function';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,11 @@ export default defineConfig({
       'import.meta.vitest': 'undefined',
     },
     plugins: [tailwindcss()],
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        visitor: fluidVisitor({}),
+      },
+    },
   },
 });
