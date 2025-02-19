4c6cb618cd115b22e1d994529a48971ae6bb91fd
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-02-19 18:52:18 +0900

    update

--- postcss.config.js
+++ postcss.config.js
@@ -34,3 +34,30 @@ export default {
     },
   },
 };
+
+function customFunctions(customFunctions) {}
+
+customFunctions({
+  '--fluid': (
+    minSize,
+    maxSize,
+    minBreakpoint = 'var(--breakpoint-sm)',
+    maxBreakpoint = 'var(--breakpoint-xl)',
+    ...rest
+  ) => {
+    if (!minSize || !maxSize) {
+      throw new Error('The --fluid(…) function requires 2 arguments, but received none.');
+    }
+
+    if (rest.length > 0) {
+      throw new Error(
+        `The --fluid(…) function only accepts 4 arguments, but received ${rest.length + 1}.`,
+      );
+    }
+
+    const slope = `calc(tan(atan2(${maxSize} - ${minSize}, 1px)) / tan(atan2(${maxBreakpoint} - ${minBreakpoint}, 1px)))`;
+    const intercept = `calc(tan(atan2(${minSize}, 1px)) - ${slope} * tan(atan2(${minBreakpoint}, 1px)))`;
+
+    return `clamp(min(${minSize}, ${maxSize}), ${slope} * 100vi + ${intercept} / 16 * 1rem, max(${minSize}, ${maxSize}))`;
+  },
+});
