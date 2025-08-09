ae5a86cf4dd9ab903fb4e9b04e37d15141e0d9e9
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2025-08-09 12:35:56 +0900

    fix vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
+  // @ts-expect-error Type error caused by different Vite versions used by Vitest and Astro dependencies
   test: {
     includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
   },
